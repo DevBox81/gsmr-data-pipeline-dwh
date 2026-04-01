4ab4250dbb28b6376f062b2a8af4289316378e32
DevBox <154363224+DevBox81@users.noreply.github.com>
2026-04-01 16:31:26 +0100

    Create init.database.sql

---
+++ scripts/init.database.sql
@@ -0,0 +1,9 @@
+/*
+This script is the start of the project, it creates the database and the schemas of each layer "bronze", "silver", "gold"
+*/
+
+CREATE DATABASE gsmr_dwh;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
